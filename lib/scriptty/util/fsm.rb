b8910fc6fc8630ed0802c4859324a35bb9b29dec
Dwayne Litzenberger <dlitz@infonium.ca>
2010-03-03 11:21:44 -0500

    Add ScripTTY::Util::FSM

---
+++ lib/scriptty/util/fsm.rb
@@ -0,0 +1,164 @@
+# Finite state machine for terminal emulation
+# Copyright (C) 2010  Infonium Inc
+#
+# This file is part of ScripTTY.
+#
+# ScripTTY is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ScripTTY is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ScripTTY.  If not, see <http://www.gnu.org/licenses/>.
+
+module ScripTTY
+  module Util
+    class FSM
+      # Exception for no matching state
+      class NoMatch < ArgumentError
+        attr_reader :state, :input
+        def initialize(message, state, input)
+          @state = state
+          @input = input
+          super(message)
+        end
+      end
+
+      # The current (or previous) input.
+      attr_reader :input
+
+      # An array of inputs received since the initial state.
+      #
+      # This allows a callback to get the contents of a complete escape
+      # sequence.
+      attr_reader :input_sequence
+
+      # The current state
+      attr_reader :state
+
+      # The next state
+      attr_reader :next_state
+
+      # Object that will receive named events.
+      #
+      # This can be a Proc object, or any object that responds to the 'call'
+      # method.
+      #
+      # When processing reaches a named event, the FSM will invoke the 'call'
+      # method on this object, passing it the name of the event and the FSM
+      # object.
+      attr_accessor :callback
+
+      # When not nil, all inputs are redirected, bypassing normal processing
+      # (but input_sequence is still updated).
+      #
+      # If redirect is a symbol, then the specified method is called on the
+      # callback object (this implies that the callback object can't be an
+      # ordinary Proc in this case).  Otherwise, the "call" method on the
+      # redirect object is invoked.
+      #
+      # The redirect function will be passed a reference to the FSM, which it
+      # can use to access methods such as input, input_sequence, reset!, etc.
+      #
+      # If the redirect function returns true, the process method returns
+      # immediately.  If the redirect function returns false, the redirection
+      # is removed and the current input is processed normally.
+      attr_accessor :redirect
+
+      # Initialize a FSM
+      #
+      # The following options are supported:
+      # [:definition]
+      #   FSM definition, as a string
+      # [:callback]
+      #   See the documentation for the callback attribute.
+      #   A block may be given to the new method instead of being passed as an
+      #   option.
+      def initialize(options={}, &block)
+        @redirect = nil
+        @input_sequence = []
+        @callback = options[:callback] || block
+        load_definition(options[:definition])
+        reset!
+      end
+
+      # Set state and next_state to 1, and clear the redirect.
+      def reset!
+        @state = 1
+        @next_state = 1
+        @redirect = nil
+        nil
+      end
+
+      # Process the specified input.
+      #
+      # If there is no matching entry in the state transition table,
+      # ScripTTY::Util::FSM::NoMatch is raised.
+      def process(input)
+        # Switch to @next_state
+        @state = @next_state
+
+        # Add the input to @input_sequence
+        if @state == 1
+          @input_sequence = [input]
+        else
+          @input_sequence << input
+        end
+
+        # Set @input and call the redirect object (if necessary)
+        @input = input
+        if @redirect
+          if @redirect.is_a?(Symbol)
+            result = @callback.send(@redirect, self)
+          else
+            result = @redirect.call(self)
+          end
+          return true if result
+          @redirect = nil
+        end
+
+        # The redirect function might invoke the reset! method, so fix
+        # @input_sequence for that case.
+        @input_sequence = [input] if @state == 1
+
+        # Look up for a state transition for the specified input
+        t = @state_transitions[@state][input]
+        t ||= @state_transitions[@state][:any]
+        raise NoMatch.new("No matching transition for state=#{@state.inspect}, input=#{input.inspect}", state, input) unless t
+
+        # Set next_state and invoke the callback, if any is specified for this state transition.
+        @next_state = t[:next_state]
+        if @callback and t[:event]
+          @callback.call(t[:event], self)
+        end
+
+        # Return true
+        true
+      end
+
+      private
+
+      # Load the specified FSM definition
+      def load_definition(definition)
+        # NB: We convert the specified state transition table into nested hashes (for faster lookups).
+        transitions = {}
+        DefinitionParser.new.parse(definition).each do |e|
+          state = e.delete(:state)
+          input = e.delete(:input)
+          raise "BUG" if !state or !input
+          e[:event] = e.delete(:event_name).to_sym if e[:event_name]   # Replace string event_name with symbol
+          transitions[state] ||= {}
+          transitions[state][input] = e
+        end
+        @state_transitions = transitions
+      end
+    end
+  end
+end
+
+require 'scriptty/util/fsm/definition_parser'
---
+++ test/fsm_test.rb
@@ -0,0 +1,231 @@
+# Tests for ScripTTY::Util::FSM
+# Copyright (C) 2010  Infonium Inc
+#
+# This file is part of ScripTTY.
+#
+# ScripTTY is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ScripTTY is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ScripTTY.  If not, see <http://www.gnu.org/licenses/>.
+
+require File.dirname(__FILE__) + "/test_helper.rb"
+require 'scriptty/util/fsm'
+
+class FSMTest < Test::Unit::TestCase
+
+  # Test the basic operation of the FSM.
+  def test_simple
+    fsm = ScripTTY::Util::FSM.new(:definition => <<-EOF)
+      'a' => eh
+      'x' => {
+        'y' => ex_why
+      }
+    EOF
+
+    fsm.process("a")
+    assert_equal 'a', fsm.input
+    assert_equal ['a'], fsm.input_sequence
+
+    fsm.process("x")
+    assert_equal 'x', fsm.input
+    assert_equal ['x'], fsm.input_sequence
+
+    fsm.process("y")
+    assert_equal 'y', fsm.input
+    assert_equal ['x', 'y'], fsm.input_sequence
+  end
+
+  # The FSM should accept more than just single characters as inputs.
+  def test_string_inputs
+    fsm = ScripTTY::Util::FSM.new(:definition => <<-EOF)
+      "inputs" => {
+        "need not" => {
+          "be" => {
+            "single characters" => dummy
+          }
+        }
+      }
+    EOF
+
+    # The FSM should treat inputs as discrete units, not try to split them up
+    # into separate characters.
+    assert_raise ScripTTY::Util::FSM::NoMatch do
+      fsm.process("i")
+    end
+
+    # Test with longer words
+    fsm.process("inputs")
+    fsm.process("need not")
+    fsm.process("be")
+    fsm.process("single characters")
+    assert_equal ["inputs", "need not", "be", "single characters"], fsm.input_sequence
+  end
+
+  # Test that the FSM accepts a callback passed as a block to the constructor.
+  def test_callback_given_as_block
+    result = []
+    fsm = ScripTTY::Util::FSM.new(:definition => <<-EOF) { |event, fsm| result << [event, fsm.input_sequence] }
+      'a' => eh
+      'x' => {
+        'y' => ex_why
+      }
+    EOF
+    fsm.process('a')
+    assert_equal [[:eh, ['a']]], result
+
+    fsm.process('x')
+    assert_equal [[:eh, ['a']]], result
+
+    fsm.process('y')
+    assert_equal [[:eh, ['a']], [:ex_why, ['x', 'y']]], result
+  end
+
+  # Test that the FSM accepts a callback passed as an option to the constructor.
+  def test_callback_given_as_option
+    result = []
+    callback_proc = Proc.new { |event, fsm| result << [event, fsm.input_sequence] }
+    fsm = ScripTTY::Util::FSM.new(:definition => <<-EOF, :callback => callback_proc)
+      'a' => eh
+      'x' => {
+        'y' => ex_why
+      }
+    EOF
+    fsm.process('a')
+    assert_equal [[:eh, ['a']]], result
+
+    fsm.process('x')
+    assert_equal [[:eh, ['a']]], result
+
+    fsm.process('y')
+    assert_equal [[:eh, ['a']], [:ex_why, ['x', 'y']]], result
+  end
+
+  def test_intermediate_callback_with_redirect_proc
+    callback_class = Class.new do
+      attr_accessor :result
+      alias call send
+      def csi(f)
+        f.redirect = lambda {|m| m.input =~ /[\d;]/}
+      end
+      def sgr(f)
+        @result ||= []
+        @result << f.input_sequence
+      end
+      def five(f)
+        @result ||= []
+        @result << :five
+      end
+    end
+    cb = callback_class.new
+    fsm = ScripTTY::Util::FSM.new(:callback => cb, :definition => <<-EOF)
+      '\e' => {
+        '[' => csi => {
+          'm' => sgr
+        }
+      }
+      '5' => five
+    EOF
+    "\e[33;42m".split("").each {|c| fsm.process(c)}
+    fsm.process("5")
+    assert_equal [["\e", "[", "3", "3", ";", "4", "2", "m"], :five], cb.result
+  end
+
+  def test_intermediate_callback_with_redirect_symbol
+    callback_class = Class.new do
+      attr_accessor :result
+      alias call send
+      def csi(f)
+        f.redirect = :csi_redirect
+      end
+      def csi_redirect(f)
+        f.input =~ /[\d;]/
+      end
+      def sgr(f)
+        @result ||= []
+        @result << f.input_sequence
+      end
+      def five(f)
+        @result ||= []
+        @result << :five
+      end
+    end
+    cb = callback_class.new
+    fsm = ScripTTY::Util::FSM.new(:callback => cb, :definition => <<-EOF)
+      '\e' => {
+        '[' => csi => {
+          'm' => sgr
+        }
+      }
+      '5' => five
+    EOF
+    "\e[33;42m".split("").each {|c| fsm.process(c)}
+    fsm.process("5")
+    assert_equal [["\e", "[", "3", "3", ";", "4", "2", "m"], :five], cb.result
+  end
+
+  def test_reset
+    fsm = ScripTTY::Util::FSM.new(:definition => <<-EOF)
+      'a' => {
+        'b' => ab
+      }
+    EOF
+    assert_equal 1, fsm.next_state
+
+    fsm.process("a")
+    assert_not_equal 1, fsm.next_state
+
+    fsm.reset!
+    assert_equal 1, fsm.next_state
+
+    assert_raise ScripTTY::Util::FSM::NoMatch do
+      fsm.process("b")
+    end
+  end
+
+  def test_reset_during_callback
+    callback_class = Class.new do
+      attr_accessor :result
+      alias call send
+      def redirect_true(f)
+        @result ||= [] ; @result << :redirect_true
+        f.reset!
+        true
+      end
+      def redirect_false(f)
+        @result ||= [] ; @result << :redirect_false
+        f.reset!
+        false
+      end
+      def x(f)
+        @result ||= [] ; @result << :x
+      end
+    end
+    cb = callback_class.new
+    fsm = ScripTTY::Util::FSM.new(:callback => cb, :definition => <<-EOF)
+      'x' => x
+    EOF
+    assert_equal 1, fsm.next_state
+
+    cb.result = []
+    fsm.redirect = :redirect_true
+    fsm.process("x")
+    assert_equal [:redirect_true], cb.result
+    assert_equal 1, fsm.next_state
+    assert_nil fsm.redirect
+
+    cb.result = []
+    fsm.redirect = :redirect_false
+    fsm.process("x")
+    assert_equal [:redirect_false, :x], cb.result
+    assert_equal 1, fsm.next_state
+    assert_nil fsm.redirect
+  end
+end
